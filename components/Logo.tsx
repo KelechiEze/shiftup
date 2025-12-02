fb44e33c19379848667f9a5ca7449131270a7f21
KelechiEze <kelechieze2000@gmail.com>
2025-12-02 23:50:39 +0100

    yes

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 
 const Logo: React.FC = () => {
   return (
-    <div className="flex items-center font-bold text-2xl tracking-tight select-none cursor-pointer">
-      <span>SH</span>
-      <div className="relative mx-[1px]">
-        {/* The 'I' */}
-        <span>I</span>
-        {/* The yellow arrow specifically placed above the I */}
-        <div className="absolute -top-1.5 left-1/2 -translate-x-1/2 w-0 h-0 border-l-[4px] border-l-transparent border-r-[4px] border-r-transparent border-b-[6px] border-b-brand-yellow"></div>
+    <div className="flex items-baseline group font-bold text-2xl tracking-tight select-none cursor-pointer leading-none">
+      <div className="flex items-center">
+        <span>SH</span>
+        <div className="relative mx-[1px]">
+          {/* The 'I' */}
+          <span>I</span>
+          {/* The yellow arrow specifically placed above the I */}
+          <div className="absolute -top-1.5 left-1/2 -translate-x-1/2 w-0 h-0 border-l-[4px] border-l-transparent border-r-[4px] border-r-transparent border-b-[6px] border-b-brand-yellow"></div>
+        </div>
+        <span>FTUP</span>
       </div>
-      <span>FTUP</span>
-      <span className="ml-1.5 font-normal text-sm uppercase tracking-wide opacity-80 pt-1">AFRICA</span>
+      
+      <div className="flex flex-col items-start ml-1.5">
+        <span className="font-normal text-sm uppercase tracking-wide opacity-90 leading-none">AFRICA</span>
+        <span className="font-serif italic text-[0.5rem] md:text-[0.6rem] tracking-wider opacity-80 mt-1 whitespace-nowrap">
+          ..A Life Of Great Possibilities
+        </span>
+      </div>
     </div>
   );
 };
